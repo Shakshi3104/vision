a1f0fbec3ca660c466bddd6723cf2e4bd6dee1a8
user <watchosastrom3104@gmail.com>
2020-07-21 17:43:45 +0900

    change default color

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import cv2
 import pandas as pd
 
 
-def plot_calibration_point(image, u, v, point_id="0", radius=10, color=(0, 188, 255)):
+def plot_calibration_point(image, u, v, point_id="0", radius=10, color=(156, 157, 154)):
     # (u, v)に円をかく
     img = cv2.circle(image, (u, v), radius, color=color, thickness=3)
     textsize = cv2.getTextSize(point_id, cv2.FONT_HERSHEY_SIMPLEX, 5, 5)[0]
@@ -29,7 +29,7 @@ def plot_calibration_point(image, u, v, point_id="0", radius=10, color=(0, 188, 
 
 
 def plot_calibration_points(read_image_path: str, write_image_path: str, points: pd.DataFrame, radius=10,
-                            color=(0, 188, 255)):
+                            color=(242, 236, 167)):
     img = cv2.imread(read_image_path)
     for row in points.itertuples():
         img = plot_calibration_point(img, row[1], row[2], str(row[0] + 1), radius, color)
